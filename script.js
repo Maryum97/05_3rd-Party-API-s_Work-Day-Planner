4387a2610ac5348e4a04a5a905bb2731572465ec
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-01-14 16:52:21 +0000

    edited script and html

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 $(document).ready(function() {
 
+    // Click save button and store to local storage
+    $("#saveBtn").on("click", function() {
+        var value = $(this).siblings("text-entry").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, value);
+    })
+
+    // Update the time and time code of planner rows
+    function hourUpdater() {
+        // get current number of hours
+        var currentHour = moment().hours();
+    
+        // loop over time blocks
+        $("#timeId").each(function() {
+          var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+          // check if we've moved past this time, so change the class wrt time
+          if (blockHour < currentHour) {
+            $(this).addClass("past");
+          } 
+          else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+          } 
+          else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+          }
+        });
+      }
+
+      hourUpdater();
+
+      
+
   })
+
