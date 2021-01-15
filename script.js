d1caf215dce3df6edfd30339efac2ab5708af845
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-01-15 16:23:59 +0000

    made some more edits

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ var currentDate = moment().format("dddd LL");
 
     var now = new Date().getHours();
 
-// Defning and logging the main elements in the scheduler
-var hourId = $(".hour");
-console.log(hourId);
-
-var textArea = $(".text-area");
-console.log(textArea);
-
-var saveBtn = $(".saveBtn");
-console.log(saveBtn);
-
-var calendarRow = $(".calendar-row");
-console.log(calendarRow);
-
 // Getting the document ready for jQuery
 $(document).ready(function() {
+
+  // Defning and logging the main elements in the scheduler
+  var hourId = $(".hour");
+  console.log(hourId);
+
+  var textArea = $(".text-area");
+  console.log(textArea);
+
+  var saveBtn = $(".saveBtn");
+  console.log(saveBtn);
+
+  var calendarRow = $(".calendar-row");
+  console.log(calendarRow);
 
 // Add event listener to saveBtn
   saveBtn.on("click", function() {
