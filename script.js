015243d9a56d31c377e8555e4b91d2fa316c496c
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-01-15 15:12:28 +0000

    edited script

--- script.js
+++ script.js
@@ -39,12 +39,42 @@ console.log(calendarRow);
     var timeNow = moment().hour();
     console.log(timeNow);
 
-    // Loop over calandar rows
+    // Loop over calandar rows to get block time
     calendarRow.each(function() {
       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
       console.log(blockTime);
     })
 
+      // Check time and set classes for background color indicators
+      if (blockTime < timeNow) {
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+          $(this).addClass("past");
+      }
+      else if (blockTime === timeNow) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+      }
+      else {
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+          $(this).addClass("future");
+
+      }
+
+      // Get item from local storage if any
+      $("#hour9 .calendar-row").val(localStorage.getItem("hour9"));
+      $("#hour10 .calendar-row").val(localStorage.getItem("hour10"));
+      $("#hour11 .calendar-row").val(localStorage.getItem("hour11"));
+      $("#hour12 .calendar-row").val(localStorage.getItem("hour12"));
+      $("#hour13 .calendar-row").val(localStorage.getItem("hour13"));
+      $("#hour14 .calendar-row").val(localStorage.getItem("hour14"));
+      $("#hour15 .calendar-row").val(localStorage.getItem("hour15"));
+      $("#hour16 .calendar-row").val(localStorage.getItem("hour16"));
+      $("#hour17 .calendar-row").val(localStorage.getItem("hour17"));
+
+      timeTracker();
 
   }
 
