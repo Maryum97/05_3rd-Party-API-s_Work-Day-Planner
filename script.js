88fc6ce642f4dc3bf07757ccbd4a73a38375a10d
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-01-15 14:07:16 +0000

    corrected html and re-set js

--- script.js
+++ script.js
@@ -1,52 +1,20 @@
-// Storing tasks to local storage
-let hourlyTasks = {
-    "9": [],
-    "10": [],
-    "11": [],
-    "12": [],
-    "13": [],
-    "14": [],
-    "15": [],
-    "16": [],
-    "17": [] 
-}
+// Display current date and time in the header or the navbar
+var currentDate = moment().format("dddd LL");
+    console.log(currentDate);
+    $("#current-date").append(currentDate);
 
-$(document).ready(function() {
+    var currentTime = moment().format("LT");
+    console.log(currentTime);
+    $("#current-time").append(currentTime);
 
-    // Click save button and store to local storage
-    $("#saveBtn").on("click", function() {
+    var now = new Date().getHours();
 
-        localStorage.setItem("hourlyTasks", JSON.stringify(hourlyTasks));
-    })
+// Defning and logging the main elements in the scheduler
+var hourId = $(".hour");
+console.log(hourId);
 
-    // Update the time and time code of planner rows
-    function hourUpdater() {
-        // get current number of hours
-        var currentHour = moment().hours();
-    
-        // loop over time blocks
-        $("#timeId").each(function() {
-          var blockHour = parseInt($(this).attr("id").split("-")[1]);
-    
-          // check if we've moved past this time, so change the class wrt time
-          if (blockHour < currentHour) {
-            $(this).addClass("past");
-          } 
-          else if (blockHour === currentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-          } 
-          else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-          }
-        });
-      }
+var textArea = $(".text-area");
+console.log(textArea);
 
-      hourUpdater();
-
-
-
-  })
-
+var saveBtn = $(".saveBtn");
+console.log(saveBtn);
