9578226f552ee80afc4bdbba37ae6bec93be00ca
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-01-16 15:28:27 +0000

    made edits to classes and script for color codes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Display current date and time in the header or the navbar
 var currentDate = moment().format("dddd LL");
-    console.log(currentDate);
-    $("#current-date").append(currentDate);
+console.log(currentDate);
+$("#current-date").append(currentDate);
 
-    var currentTime = moment().format("LT");
-    console.log(currentTime);
-    $("#current-time").append(currentTime);
+var currentTime = moment().format("LT");
+console.log(currentTime);
+$("#current-time").append(currentTime);
 
-    var now = new Date().getHours();
+var now = new Date().getHours();
 
-// Getting the document ready for jQuery
-$(document).ready(function() {
+// Getting functions ready
+window.onload = function() {
+  trackTime()
+}
 
-  // Defning and logging the main elements in the scheduler
+
+  // Defining and logging the main elements in the scheduler
   var hourId = $(".hour");
   console.log(hourId);
-
-  var textArea = $(".text-area");
-  console.log(textArea);
 
   var saveBtn = $(".saveBtn");
   console.log(saveBtn);
@@ -27,41 +27,112 @@ $(document).ready(function() {
 
 // Add event listener to saveBtn
   saveBtn.on("click", function() {
-    var taskText = $(this).siblings(textArea);
+    var taskText = $(this).siblings(textArea).val();
     var time = $(this).parent().attr("id");
 
     // Save to local storage
     localStorage.setItem(time, taskText);
   })
 
+  // Setting the classes for each time block on the scheduler
   function trackTime() {
-    // Get current hours and log
+
+    // Get current hours 
     var timeNow = moment().hour();
 
-    // Loop over calandar rows to get block time
-    calendarRow.each(function() {
-      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-      // Check time and set classes for background color indicators
-      if (blockTime < timeNow) {
-          $(this).removeClass("future");
-          $(this).removeClass("present");
-          $(this).addClass("past");
+      // Check if time NOW matches time of time-block, and assign classes for background color indicators
+      if (timeNow > 9) {
+        $(".text-area-1").addClass("past");
       }
-      else if (blockTime === timeNow) {
-          $(this).removeClass("past");
-          $(this).removeClass("future");
-          $(this).addClass("present");
+      else if (timeNow === 9) {
+        $(".text-area-1").addClass("present");
       }
-      else {
-          $(this).removeClass("present");
-          $(this).removeClass("past");
-          $(this).addClass("future");
+      else if (timeNow < 9) {
+        $(".text-area-1").addClass("future");
+      }
 
+      if (timeNow > 10) {
+        $(".text-area-2").addClass("past");
+      }
+      else if (timeNow === 10) {
+        $(".text-area-2").addClass("present");
+      }
+      else if (timeNow < 10) {
+        $(".text-area-2").addClass("future");
+      }
+
+      if (timeNow > 11) {
+        $(".text-area-3").addClass("past");
+      }
+      else if (timeNow === 11) {
+        $(".text-area-3").addClass("present");
+      }
+      else if (timeNow < 11) {
+        $(".text-area-3").addClass("future");
+      }
+
+      if (timeNow > 12) {
+        $(".text-area-4").addClass("past");
+      }
+      else if (timeNow === 12) {
+        $(".text-area-4").addClass("present");
+      }
+      else if (timeNow < 12) {
+        $(".text-area-4").addClass("future");
+      }
+
+      if (timeNow > 13) {
+        $(".text-area-5").addClass("past");
+      }
+      else if (timeNow === 13) {
+        $(".text-area-5").addClass("present");
+      }
+      else if (timeNow < 13) {
+        $(".text-area-5").addClass("future");
+      }
+
+      if (timeNow > 14) {
+        $(".text-area-6").addClass("past");
+      }
+      else if (timeNow === 14) {
+        $(".text-area-6").addClass("present");
+      }
+      else if (timeNow < 14) {
+        $(".text-area-6").addClass("future");
+      }
+
+      if (timeNow > 15) {
+        $(".text-area-7").addClass("past");
+      }
+      else if (timeNow === 15) {
+        $(".text-area-7").addClass("present");
+      }
+      else if (timeNow < 15) {
+        $(".text-area-7").addClass("future");
+      }
+
+      if (timeNow > 16) {
+        $(".text-area-8").addClass("past");
+      }
+      else if (timeNow === 16) {
+        $(".text-area-8").addClass("present");
+      }
+      else if (timeNow < 16) {
+        $(".text-area-8").addClass("future");
+      }
+
+      if (timeNow > 17) {
+        $(".text-area-9").addClass("past");
+      }
+      else if (timeNow === 17) {
+        $(".text-area-9").addClass("present");
+      }
+      else if (timeNow < 17) {
+        $(".text-area-9").addClass("future");
       }
       
-    })
-  }
+    }
+  
 
       // Get item from local storage if any
       $("#hour9 .calendar-row").val(localStorage.getItem("hour9"));
@@ -75,5 +146,3 @@ $(document).ready(function() {
       $("#hour17 .calendar-row").val(localStorage.getItem("hour17"));
 
       trackTime();
-
-})
